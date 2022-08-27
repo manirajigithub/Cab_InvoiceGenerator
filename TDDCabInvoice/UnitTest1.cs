c5c4986a73e833ee57674200a91cac4d1e2b2015
manirajigithub <109149403+manirajigithub@users.noreply.github.com>
2022-08-27 16:30:24 +0530

    [Rajeswari]Calculated Fare

--- TDDCabInvoice/UnitTest1.cs
+++ TDDCabInvoice/UnitTest1.cs
@@ -8,44 +8,10 @@ namespace TDDCabInvoice
         [TestMethod]
         public void CabFare()
         {
-            CabInvoice Invoice = new CabInvoice();
-            double res = Invoice.Fare(10, 10);
-            Assert.AreEqual(110, res);
+            CabInvoice invoice = new CabInvoice();
+            double res = invoice.Fare(10,10);
+            Assert.AreEqual(110,res);
         }
-
-        [TestMethod]
-        public void MultiRideFare()
-        {
-            CabInvoice Invoice = new CabInvoice();
-            List<Rides> rides = new List<Rides>()
-            {
-                new Rides(20, 30),   //230
-                new Rides(2, 3),     //23
-                new Rides(12, 16),   //136
-                new Rides(5, 7),     //57
-                new Rides(19, 23)    //213
-        };
-            EnhancedInvoice res = Invoice.MultiRideFare(rides);
-            Assert.AreEqual(659, res.totalFare);
-        }
-        [TestMethod]
-        public void EnhancedInvoice()
-        {
-
-            CabInvoice Invoice = new CabInvoice();
-            List<Rides> rides = new List<Rides>()
-            {
-                new Rides(20, 30),   //230
-                new Rides(2, 3),     //23
-                new Rides(12, 16),   //136
-                new Rides(5, 7),     //57
-                new Rides(19, 23)    //213
-            };
-            EnhancedInvoice res = Invoice.MultiRideFare(rides);
-            Assert.AreEqual(659, res.totalFare);
-            Assert.AreEqual(5, res.totalRides);
-            Assert.AreEqual(131.8, res.AvgFare);
-        }
+        
     }
-
 }
