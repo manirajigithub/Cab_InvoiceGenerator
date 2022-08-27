7411d9175398a7396a2381cf9fb29f5bd9238919
manirajigithub <109149403+manirajigithub@users.noreply.github.com>
2022-08-27 16:42:09 +0530

    [Rajeswari]Calculated Multiple Rides

--- TDDCabInvoice/UnitTest1.cs
+++ TDDCabInvoice/UnitTest1.cs
@@ -1,4 +1,5 @@
-using System;
+using CabInvoiceGeneratorUC2;
+
 namespace TDDCabInvoice
 {
     [TestClass]
@@ -7,10 +8,25 @@ namespace TDDCabInvoice
         [TestMethod]
         public void CabFare()
         {
-            CabInvoice invoice = new CabInvoice();
-            double res = invoice.Fare(10, 10);
+            CabInvoice Invoice = new CabInvoice();
+            double res = Invoice.Fare(10, 10);
             Assert.AreEqual(110, res);
         }
 
+        [TestMethod]
+        public void MultiRideFare()
+        {
+            CabInvoice Invoice = new CabInvoice();
+            List<Rides> rides = new List<Rides>()
+            {
+                new Rides(20, 30),   //230
+                new Rides(2, 3),     //23
+                new Rides(12, 16),   //136
+                new Rides(5, 7),     //57
+                new Rides(19, 23)    //213
+        };
+            double res = Invoice.MultiRideFare(rides);
+            Assert.AreEqual(659, res);
+        }
     }
 }
